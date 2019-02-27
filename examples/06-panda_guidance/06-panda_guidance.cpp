d0bdbfa178769a51175adf0f939be51cdb9f856c
Adrian Piedra <apiedra@stanford.edu>
2019-02-26 22:41:08 -0800

    Attempting to implement momentum-based observer

--- examples/06-panda_guidance/06-panda_guidance.cpp
+++ examples/06-panda_guidance/06-panda_guidance.cpp
@@ -43,6 +43,7 @@ bool fSimulationRunning = false;
 void sighandler(int){fSimulationRunning = false;}
 
 using namespace std;
+using namespace Eigen;
 
 const string world_file = "resources/world_panda.urdf";
 const string robot_file = "resources/panda_arm.urdf";
@@ -56,7 +57,8 @@ void simulation(Sai2Model::Sai2Model* robot1, Sai2Model::Sai2Model* robot2, Forc
 
 // control link and position in link
 const string link_name = "link7";
-const Eigen::Vector3d pos_in_link = Eigen::Vector3d(0.0,0.0,0.20);
+//const Eigen::Vector3d pos_in_link = Eigen::Vector3d(0.0,0.0,0.20);
+const Eigen::Vector3d pos_in_link = Eigen::Vector3d(0.0,0.0,0.1);
 const Eigen::Vector3d sensor_pos_in_link = Eigen::Vector3d(0.0,0.0,0.05);
 Eigen::Vector3d sensed_force;
 Eigen::Vector3d sensed_moment;
@@ -261,6 +263,7 @@ int main (int argc, char** argv) {
 	fSimulationRunning = false;
 	sim_thread.join();
 	ctrl_thread.join();
+//    auto pos_task = new Sai2Primitives::PosOriTask(robot1, link_name, pos_in_link);
 
     // destroy context
     glfwDestroyWindow(window);
@@ -310,6 +313,7 @@ void recordToCSV(Eigen::VectorXd &v, const std::string &filename)
 ========================================================================================== */
 void control(Sai2Model::Sai2Model* robot1, Sai2Model::Sai2Model* robot2, Simulation::Sai2Simulation* sim) {
 	
+    // prepare controller
     robot1->updateModel();
     robot2->updateModel();
     int dof = robot1->dof();
@@ -321,12 +325,49 @@ void control(Sai2Model::Sai2Model* robot1, Sai2Model::Sai2Model* robot2, Simulat
 	Eigen::Affine3d sensor_frame_in_link = Eigen::Affine3d::Identity();
 	sensor_frame_in_link.translation() = pos_in_link;
 
-	// Motion arm primitive - for initial alignment
+    Eigen::MatrixXd N_prec = Eigen::MatrixXd::Identity(dof, dof);
+
+    // Motion arm primitive - for initial alignment
     Sai2Primitives::RedundantArmMotion* motion_primitive1 = new Sai2Primitives::RedundantArmMotion(robot1, link_name, pos_in_link);
     Sai2Primitives::RedundantArmMotion* motion_primitive2 = new Sai2Primitives::RedundantArmMotion(robot2, link_name, pos_in_link);
     motion_primitive1->enableGravComp();
     motion_primitive2->enableGravComp();
 
+    // joint tasks
+    auto joint_task1 = new Sai2Primitives::JointTask(robot1);
+    auto joint_task2 = new Sai2Primitives::JointTask(robot2);
+
+    Eigen::VectorXd joint_task_torques1 = Eigen::VectorXd::Zero(dof);
+    Eigen::VectorXd joint_task_torques_np1 = Eigen::VectorXd::Zero(dof);
+//    motion_primitive1->_joint_task->_kp = 50.0;
+//    motion_primitive1->_joint_task->_kv = 11.0;
+//    motion_primitive1->_joint_task->_ki = 50.0;
+
+    Eigen::VectorXd joint_task_torques2 = Eigen::VectorXd::Zero(dof);
+    Eigen::VectorXd joint_task_torques_np2 = Eigen::VectorXd::Zero(dof);
+    joint_task2->_kp = 50.0;
+    joint_task2->_kv = 11.0;
+    joint_task2->_ki = 50.0;
+
+//    VectorXd q_init_desired = VectorXd::Zero(dof);
+//    // q_init_desired << 0, 0, 0, -10, 0, 90, 0;
+//    q_init_desired << 0, 15, 0, -95, 0, 125, 0;
+//    q_init_desired *= M_PI/180;
+//    motion_primitive1->_joint_task->_desired_position = q_init_desired;
+
+    // pos tasks
+//    auto motion_primitive1->_posori_task = new Sai2Primitives::PosOriTask(robot1, link_name, pos_in_link);
+    auto pos_task2 = new Sai2Primitives::PosOriTask(robot2, link_name, pos_in_link);
+
+    VectorXd pos_task_torques1 = VectorXd::Zero(dof);
+//    motion_primitive1->_posori_task->_kp_pos = 300.0;
+//    motion_primitive1->_posori_task->_kv_pos = 25.0;
+    // pos_task->_ki = 50.0;
+
+    VectorXd pos_task_torques2 = VectorXd::Zero(dof);
+    pos_task2->_kp_pos = 300.0;
+    pos_task2->_kv_pos = 25.0;
+    // pos_task->_ki = 50.0;
 
 	// Screwing primitive 
     Sai2Primitives::ScrewingAlignment* screwing_primitive1 = new Sai2Primitives::ScrewingAlignment(robot1, link_name, control_frame_in_link, sensor_frame_in_link);
@@ -338,10 +379,87 @@ void control(Sai2Model::Sai2Model* robot1, Sai2Model::Sai2Model* robot2, Simulat
 	Eigen::Matrix3d initial_orientation;
     Eigen::Vector3d initial_position1;
     Eigen::Vector3d initial_position2;
-    robot1->rotation(initial_orientation, motion_primitive1->_link_name);
-    robot1->position(initial_position1, motion_primitive1->_link_name, motion_primitive1->_control_frame.translation());
-    robot2->rotation(initial_orientation, motion_primitive2->_link_name);
-    robot2->position(initial_position2, motion_primitive2->_link_name, motion_primitive2->_control_frame.translation());
+//    robot1->rotation(initial_orientation, motion_primitive1->_link_name);
+//    robot1->position(initial_position1, motion_primitive1->_link_name, motion_primitive1->_control_frame.translation());
+//    robot2->rotation(initial_orientation, motion_primitive2->_link_name);
+//    robot2->position(initial_position2, motion_primitive2->_link_name, motion_primitive2->_control_frame.translation());
+    robot1->rotation(initial_orientation, motion_primitive1->_posori_task->_link_name);
+    robot1->position(initial_position1, motion_primitive1->_posori_task->_link_name, motion_primitive1->_posori_task->_control_frame.translation());
+    robot2->rotation(initial_orientation, pos_task2->_link_name);
+    robot2->position(initial_position2, pos_task2->_link_name, pos_task2->_control_frame.translation());
+
+    // prepare observers
+    VectorXd gravity1(dof), gravity2(dof), coriolis1(dof), coriolis2(dof);
+    VectorXd r_mom = VectorXd::Zero(dof);
+    VectorXd p = VectorXd::Zero(dof);
+    VectorXd beta = VectorXd::Zero(dof);
+    MatrixXd M_prev = MatrixXd::Zero(dof,dof);
+    MatrixXd M_dot = MatrixXd::Zero(dof,dof);
+    VectorXd integral_mom = VectorXd::Zero(dof);
+
+    VectorXd K = VectorXd::Zero(dof);
+    // K << 25, 25, 25, 25, 25, 25, 25;
+    // K << 10, 15, 20, 25, 30, 50, 80;
+    // K << 25, 25, 25, 25, 15, 10, 5;
+    K << 2, 2, 2, 2, 1, 1, 1;
+    MatrixXd K0 = MatrixXd::Identity(dof,dof);
+    for(int i=0 ; i<dof ; i++)
+    {
+        K0(i,i) = K(i);
+    }
+    bool first_iteration = true;
+    double contact_detection_treshold = 1;
+    double link_detection_treshold = 0.15;
+    int link_in_contact = -1;
+    VectorXd r_filtered = VectorXd::Zero(dof);
+    bool in_contact = false;
+    bool in_contact_prev = false;
+
+    MatrixXd Lambda_contact = MatrixXd::Zero(1,1);
+    MatrixXd Jbar_contact = MatrixXd::Zero(dof,1);
+    MatrixXd Jbar_contact_np = MatrixXd::Zero(dof,1);
+    MatrixXd J_contact = MatrixXd::Zero(1,dof);
+    MatrixXd J_contact_control = MatrixXd::Zero(1,dof);
+    MatrixXd N_contact = MatrixXd::Identity(dof,dof);
+
+    ButterworthFilter filter_r = ButterworthFilter(dof, 0.015);
+
+    // link centers
+    MatrixXd J_sample = MatrixXd::Zero(3,dof);
+    MatrixXd Lambda_sample = MatrixXd::Zero(3,3);
+    MatrixXd N_sample = MatrixXd::Zero(dof,dof);
+    MatrixXd Jbar_sample = MatrixXd::Zero(dof,3);
+    double J_norm_estimate = 1;
+
+    vector<string> link_names;
+    link_names.push_back("link1");
+    link_names.push_back("link2");
+    link_names.push_back("link3");
+    link_names.push_back("link4");
+    link_names.push_back("link5");
+    link_names.push_back("link6");
+    link_names.push_back("link7");
+
+    vector<Vector3d> center_points;
+    center_points.push_back(Vector3d(0.0, 0.0, -0.1));
+    center_points.push_back(Vector3d(0.0, -0.07, 0.0));
+    center_points.push_back(Vector3d(0.0, 0.0, -0.07));
+    center_points.push_back(Vector3d(-0.088, 0.05, 0.0));
+    center_points.push_back(Vector3d(0.0, 0.0, -0.1));
+    center_points.push_back(Vector3d(0.088, 0.0, 0.0));
+    center_points.push_back(Vector3d(0.0, 0.0, 0.075));
+
+    int r_buffer_length = 20;
+    MatrixXd r_buffer = MatrixXd::Zero(dof,r_buffer_length);
+    VectorXd r_norm_buffer = VectorXd::Zero(r_buffer_length);
+    VectorXd r_link_in_contact_buffer = -1*VectorXd::Ones(r_buffer_length);
+    VectorXd r_link_in_contact_count = VectorXd::Zero(dof);
+    VectorXd r_mean = VectorXd::Zero(dof);
+
+    // torques to compensate contact at task level
+    VectorXd contact_compensation_torques = VectorXd::Zero(dof);
+    VectorXd contact_force_control_torques = VectorXd::Zero(dof);
+    double direction = 0;
 
 	// create a loop timer
 	double control_freq = 1000;
@@ -363,13 +481,21 @@ void control(Sai2Model::Sai2Model* robot1, Sai2Model::Sai2Model* robot2, Simulat
 		double loop_dt = curr_time - last_time;
 		double time = controller_counter/control_freq;
 
-		// read joint positions, velocities, update model
+        M_prev = robot1->_M;
+
+        // read joint positions, velocities, update model + gravity + coriolis
         sim->getJointPositions(robot1_name, robot1->_q);
         sim->getJointVelocities(robot1_name, robot1->_dq);
         sim->getJointPositions(robot2_name, robot2->_q);
         sim->getJointVelocities(robot2_name, robot2->_dq);
         robot1->updateModel();
+        robot1->gravityVector(gravity1);
+        robot1->coriolisForce(coriolis1);
         robot2->updateModel();
+        robot2->gravityVector(gravity2);
+        robot2->coriolisForce(coriolis2);
+
+        M_dot = (robot1->_M - M_prev) / (loop_dt);
 
 		// update tasks model
         screwing_primitive1->updatePrimitiveModel(); //ADDED
@@ -399,6 +525,9 @@ void control(Sai2Model::Sai2Model* robot1, Sai2Model::Sai2Model* robot2, Simulat
         motion_primitive1->_desired_position = initial_position1 + goal_delPos1;
         motion_primitive1->_desired_velocity = Eigen::Vector3d(0,0,0);
 
+//        motion_primitive1->_posori_task->_desired_position = initial_position1 + goal_delPos1;
+//        motion_primitive1->_posori_task->_desired_velocity = Eigen::Vector3d(0,0,0);
+
         // set follower position (under collision or in free space)
         robot2->position(curr_Pos2, motion_primitive2->_link_name, motion_primitive2->_control_frame.translation());
         // check for collision
@@ -415,11 +544,178 @@ void control(Sai2Model::Sai2Model* robot1, Sai2Model::Sai2Model* robot2, Simulat
             motion_primitive2->_desired_position = initial_position2 + goal_delPos2;
         }
 
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        // NEW CODE //
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        // update tasks model
+        N_prec.setIdentity();
+        motion_primitive1->_posori_task->updateTaskModel(N_prec);
+        N_prec = motion_primitive1->_posori_task->_N;
+
+        // for direction monitoring
+        motion_primitive1->_joint_task->updateTaskModel(N_prec);
+        motion_primitive1->_joint_task->computeTorques(joint_task_torques_np1);
+
+        if(in_contact)
+        {
+            Eigen::VectorXd Fc_est = VectorXd::Zero(3);
+            if(link_in_contact == 0)
+            {
+                J_norm_estimate = 1;
+                J_contact_control = r_filtered.transpose()/r_filtered.norm() * J_norm_estimate;
+            }
+            else
+            {
+                robot1->Jv(J_sample, link_names[link_in_contact], center_points[link_in_contact]);
+                // robot->Jv(J_sample.transpose(), link_names[link_in_contact], center_points[link_in_contact]);
+                JacobiSVD<MatrixXd> svd(J_sample, ComputeFullU | ComputeFullV);
+                J_norm_estimate = svd.singularValues()(0);
+
+                Eigen::MatrixXd J_inv = MatrixXd::Zero(dof,3);
+                Eigen::MatrixXd Sigma_inv = MatrixXd::Zero(dof, 3);
+                for(int i=0 ; i<3 ; i++)
+                {
+                    if(svd.singularValues()(i) > 1e-3)
+                    {
+                        Sigma_inv(i,i) = 1/svd.singularValues()(i);
+                        // cout << svd.matrixV().col(i)*svd.matrixU().col(i).transpose()/svd.singularValues()(i) << endl;
+                        // J_inv += svd.matrixV().col(i)*svd.matrixU().col(i).transpose()/svd.singularValues()(i);
+                    }
+                }
+                J_inv = svd.matrixV() * Sigma_inv * svd.matrixU().transpose();
+                // robot->operationalSpaceMatrices(Lambda_sample, Jbar_sample, N_sample, J_sample);
+                // Fc_est = -Jbar_sample.transpose()*r_mom;
+                Fc_est = J_inv.transpose()*r_mom;
+                // Fc_est = -svd.solve(r_mom);
+                J_contact_control = Fc_est.transpose()*J_sample/Fc_est.norm();
+            }
+
+            // robot->Jv(J_sample, link_names[link_in_contact], center_points[link_in_contact]);
+            // robot->dynConsistentInverseJacobian(Jbar_sample, J_sample);
+
+            J_contact = r_filtered.transpose()/r_filtered.norm();
+            robot1->operationalSpaceMatrices(Lambda_contact, Jbar_contact_np, N_contact, J_contact, N_prec);
+            J_contact = J_contact * N_prec;
+            robot1->operationalSpaceMatrices(Lambda_contact, Jbar_contact, N_contact, J_contact, N_prec);
+            N_prec = N_prec * N_contact;
+
+
+            motion_primitive1->_joint_task->updateTaskModel(N_prec);
+        }
+
+        // compute momentum-based observer
+        if (!first_iteration)
+        {
+            p = robot1->_M * robot1->_dq;
+            beta = coriolis1 - M_dot * robot1->_dq;
+            // beta = coriolis;
+            integral_mom += (command_torques1 - beta + r_mom) * loop_dt;
+            r_mom = K0 * (p - integral_mom);
+        }
+
+        // process velocity observer
+        r_filtered = filter_r.update(r_mom);
+        int i_buffer = controller_counter % r_buffer_length;
+        r_norm_buffer(i_buffer) = r_filtered.norm();
+        in_contact_prev = in_contact;
+        if(r_link_in_contact_buffer[i_buffer] != -1)
+        {
+            r_link_in_contact_count(r_link_in_contact_buffer[i_buffer]) -= 1;
+        }
+        link_in_contact = -1;
+        for(int i=0 ; i<dof ; i++)
+        {
+            if(r_filtered(i) > link_detection_treshold || -r_filtered(i) > link_detection_treshold)
+            {
+                link_in_contact = i;
+            }
+        }
+        r_link_in_contact_buffer(i_buffer) = link_in_contact;
+        if(link_in_contact > -1)
+        {
+            r_link_in_contact_count(link_in_contact) += 1;
+        }
+
+        bool buffer_all_true = true;
+        bool buffer_all_false = true;
+
+        for (int i = 0; i < r_buffer_length; i++)
+        {
+            buffer_all_true = buffer_all_true && (r_link_in_contact_buffer(i) != -1);
+            buffer_all_false = buffer_all_false && (r_link_in_contact_buffer(i) == -1);
+        }
+
+        // for(int i=0 ; i<r_buffer_length ; i++)
+        // {
+        // 	buffer_all_true = buffer_all_true && r_norm_buffer(i) > contact_detection_treshold;
+        // 	buffer_all_false = buffer_all_false && r_norm_buffer(i) < contact_detection_treshold;
+        // }
+        if(buffer_all_true)
+        {
+            in_contact = true;
+        }
+        else if(buffer_all_false)
+        {
+            in_contact = false;
+        }
+        else
+        {
+            in_contact = in_contact_prev;
+        }
+
+        if(in_contact)
+        {
+            int lc = 0;
+            for(int i=0 ; i<dof ; i++)
+            {
+                if(r_link_in_contact_count(i) > r_link_in_contact_count(lc))
+                {
+                    lc = i;
+                }
+            }
+            link_in_contact = lc;
+        }
+
+        if(!in_contact && in_contact_prev)
+        {
+            cout << "lost contact" << endl;
+//            motion_primitive1->_joint_task->_desired_position = robot1->_q;
+        }
+
+        // compute torques
+        motion_primitive1->_posori_task->computeTorques(pos_task_torques1);
+        motion_primitive1->_joint_task->computeTorques(joint_task_torques1);
+
+        // contact_compensation_torques.setZero(dof);
+        contact_compensation_torques = motion_primitive1->_posori_task->_projected_jacobian.transpose() * motion_primitive1->_posori_task->_Jbar.transpose() * r_mom;
+        contact_force_control_torques.setZero(dof);
+        if(in_contact)
+        {
+            double target_contact_force = 0;
+            direction = (double) (Jbar_contact_np.transpose() * (pos_task_torques1 + joint_task_torques_np1))(0);
+            if(direction < 0)
+            {
+                target_contact_force = 5;
+            }
+            contact_force_control_torques = target_contact_force*J_contact_control.transpose();
+            // contact_force_control_torques = r_mom;
+        }
+
+//        command_torques1 = pos_task_torques1 + joint_task_torques1 + coriolis1 - contact_compensation_torques - contact_force_control_torques;
+         command_torques1 = pos_task_torques1 + joint_task_torques1 + coriolis1;
+
+        if(first_iteration)
+        {
+            first_iteration = false;
+        }
+
         // set follower velocity
         motion_primitive2->_desired_velocity = Eigen::Vector3d(0,0,0);
 
         // set leader torques
-        motion_primitive1->computeTorques(command_torques1);
+         motion_primitive1->computeTorques(command_torques1);
+//        cout << command_torques1 << endl;
         sim->setJointTorques(robot1_name, command_torques1);
 
         // set follower torques
@@ -430,6 +726,25 @@ void control(Sai2Model::Sai2Model* robot1, Sai2Model::Sai2Model* robot2, Simulat
     //		theta_deg = 0;
     //		return FINISHED;
     //	}
+
+        if(controller_counter % 500 == 0)
+        {
+            // cout << in_contact << endl;
+            // cout << r_filtered.transpose() * (command_torques + contact_compensation_torques) << endl;
+            cout << "filtered observer :\n" << r_filtered.transpose() << endl;
+            cout << "link in contact buffer :\n" << r_link_in_contact_buffer.transpose() << endl;
+            cout << "link in contact count :\n" << r_link_in_contact_count.transpose() << endl;
+            cout << "link in contact : " << link_in_contact << endl;
+//            cout << "direction : " << Jbar_contact_np.transpose() * (pos_task_torques + joint_task_torques_np) << endl;
+            // cout << J_contact * robot->_dq << endl;
+            // cout << "velocity based observer :\n" << r_vel.transpose() << endl;
+            // cout << "norm of r_vel : " << r_filtered.norm() << endl;
+            // cout << "sensed torques :\n" << (filtered_sensed_torques - gravity).transpose() << endl;
+            // cout << "link in contact : " << link_in_contact << endl;
+            // cout << "norm J : " << J_norm_estimate << endl;
+            cout << "position error :\n" << (motion_primitive1->_posori_task->_current_position - motion_primitive1->_posori_task->_desired_position).transpose() << endl;
+            cout << endl;
+        }
 
         // update counter and timer
 		controller_counter++;
@@ -482,7 +797,7 @@ void simulation(Sai2Model::Sai2Model* robot1, Sai2Model::Sai2Model* robot2, Forc
 		// force sensor update
 		fsensor->update(sim);
         fsensor->getForce(sensed_force);
-        cout << sensed_force << endl;
+//        cout << sensed_force << endl;
         fsensor->getMoment(sensed_moment);
 
 		// integrate forward
